29c21ffeeb619a3011473dfff5ab3bb86be17a78
Bruno Dutertre <bruno@csl.sri.com>
2010-07-07 16:52:08 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/private/yices2/branches/new-terms@1513 0c041c21-16df-db11-9388-003048836090

--- src/internalization_table.h
+++ src/internalization_table.h
@@ -6,14 +6,13 @@
 #define __INTERNALIZATION_TABLE_H
 
 /*
- * An internalization table keeps track of variable substitutions
- * and of the internal object (egraph term or other) mapped to 
- * terms. It stores a partition of term indices in a union-find data 
- * structure:
- * - All the elements of an equivalence class are equal and 
- *   mapped to the same solver object.
- * - Each class contains a distinguished root element. All elements
- *   other than the root are uninterpreted term indices (i.e., variables).
+ * An internalization table keeps track of variable substitutions and
+ * of the internal object (egraph term or other) mapped to terms.  The
+ * table stores a partition of term indices in a union-find data
+ * structure.  All the elements of an equivalence class are equal and
+ * mapped to the same solver object.  Each class contains a
+ * distinguished root element. All elements other than the root are
+ * uninterpreted term indices (i.e., variables).
  *
  * - For a root r we store: 
  *      map[r] = object mapped to the class (or NULL)
@@ -34,9 +33,9 @@
  *   parent of i in the union-find tree.
  *
  * To distinguish between roots and non-roots, we use the sign bit:
- *   map[i] < 0  if i is a root
+ *   map[i] < 0  if i is a root, and the object mapped to i is obtained
+ *               by clearing the sign bit.
  *   map[i] >= 0 if i is not a root, then map[i] is a term (term index + polarity bit)
- *
  *
  * For boolean classes, the polarity bit is significant. It's used for both
  * the terms and the internalization object:
@@ -47,9 +46,9 @@
  *    pos_term(i) --> t and neg_term(i) --> not(t).
  */
 
-
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "int_hash_sets.h"
 #include "int_queues.h"
@@ -175,7 +174,7 @@ extern type_t intern_tbl_type_of_root(intern_tbl_t *tbl, term_t t);
  * - x must be non-negative and strictly smaller than INT32_MAX
  *   the low order bit of x is interpreted as a polarity bit.
  * - r must be a root, not mapped to anything yet
- */ 
+ */
 extern void intern_tbl_map_root(intern_tbl_t *tbl, term_t r, int32_t x);
 
 
@@ -185,19 +184,19 @@ extern void intern_tbl_map_root(intern_tbl_t *tbl, term_t r, int32_t x);
  */
 
 /*
- * Check whether r is a free root
+ * Check whether r is a free root:
+ * - r must be a root
+ * - if r is in the table, return true if rank[r] < 255, 
+ *   otherwise, return true if r is uninterpreted.
  */
-static inline bool intern_tbl_root_is_free(intern_tbl_t *tbl, term_t t) {
-  assert(intern_tbl_is_root(tbl, t));
-  return au8_read(&tbl->rank, index_of(t)) < 255;
-}
+extern bool intern_tbl_root_is_free(intern_tbl_t *tbl, term_t r);
 
 
 /*
  * Check whether the substitution [r1 := r2] is valid
  * - both r1 and r2 must be roots and they must have compatible types.
  * - r2 must not be a constant term.
- * - returns true if r1 is a free root and the substitution does not 
+ * - returns true if r1 is a free root, and the substitution does not 
  *   create a cycle.
  *
  * NOTE: if r2 is a constant, the next function should be used instead.
@@ -206,9 +205,10 @@ extern bool intern_tbl_valid_subst(intern_tbl_t *tbl, term_t r1, term_t r2);
 
 
 /*
- * Check whether the substitution [r1 := r2] where r2 is a constant
- * is valid. This does the same checks as the previous function and 
- * also checks whether r2's type is a subtype of r1's type.
+ * Check whether the substitution [r1 := r2] is valid. 
+ * - r1 must be a root and r2 must be a constant
+ * - returns true if r1 is a free root and 
+ *   if r2's type is a subtype of r1's class type.
  *
  * (e.g., x := 1/2 is not a valid substitution if x is an integer variable).
  */
@@ -229,8 +229,6 @@ extern void intern_tbl_add_subst(intern_tbl_t *tbl, term_t r1, term_t r2);
  * This adds either the substitution [r1 := r2] or [r2 := r1]
  */
 extern void intern_tbl_merge_classes(intern_tbl_t *tbl, term_t r1, term_t r2);
-
-
 
 
 #endif /* __INTERNALIZATION_TABLE_H */
