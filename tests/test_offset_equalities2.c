cebe914681f22e3a67a148f0bbe84bc300b798bb
Bruno Dutertre <bruno@csl.sri.com>
2012-11-09 17:21:04 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/private/yices2/branches/new-terms@2659 0c041c21-16df-db11-9388-003048836090

--- tests/test_offset_equalities2.c
+++ tests/test_offset_equalities2.c
@@ -272,91 +272,6 @@ static void build_poly_table(poly_table_t *table, uint32_t nvars, uint32_t n) {
 
 
 /*
- * PRINT FUNCTIONS
- */
-static void print_mono(const char *prefix, rational_t *coeff, int32_t x, bool first) {
-  bool negative;
-  bool abs_one;
-
-  negative = q_is_neg(coeff);
-  if (negative) {
-    if (first) {
-      printf("- ");
-    } else {
-      printf(" - ");
-    }
-    abs_one = q_is_minus_one(coeff);
-  } else {
-    if (! first) {
-      printf(" + ");
-    }
-    abs_one = q_is_one(coeff);
-  }
-
-  if (x == const_idx) {
-    q_print_abs(stdout, coeff);
-  } else {
-    if (! abs_one) {
-      q_print_abs(stdout, coeff);
-      printf(" * ");
-    }
-    printf("%s%"PRId32, prefix, x);
-  }
-}
-
-static void print_poly(polynomial_t *p) {
-  uint32_t i, n;
-  bool first;
-
-  if (polynomial_is_zero(p)) {
-    printf("0");
-  } else {
-    n = p->nterms;
-    first = true;
-    for (i=0; i<n; i++) {
-      print_mono("x", &p->mono[i].coeff, p->mono[i].var, first);
-      first = false;
-    }
-  }
-}
-
-static void print_buffer(poly_buffer_t *b) {
-  monomial_t *mono;
-  uint32_t i, n;
-  bool first;
-
-  n = poly_buffer_nterms(b);
-  mono = poly_buffer_mono(b);
-  if (n == 0) {
-    printf("0");
-  } else {
-    first = true;
-    for (i=0; i<n; i++) {
-      print_mono("x", &mono[i].coeff, mono[i].var, first);
-      first = false;
-    }
-  }  
-}
-
-
-static void print_poly_table(poly_table_t *table) {
-  polynomial_t *p;
-  uint32_t i, n;
-
-  n = table->npolys;
-  for (i=0; i<n; i++) {
-    p = table->poly[i];
-    if (p != NULL) {
-      printf("  x%"PRIu32" := ", i);
-      print_poly(p);
-      printf("\n");
-    }
-  }
-}
-
-
-
-/*
  * SUBSTITUTIONS
  */
 
@@ -468,8 +383,8 @@ static void subst_var(substitution_t *s, offset_pair_t *d) {
   k = d->delta;
 
   while (x >= 0 && s->elim[x]) {
-    x = s->var[x];
     k += s->delta[x];
+    x = s->var[x];
     n --;
     if (n == 0) goto bug;      
   }
@@ -618,7 +533,6 @@ static void free_normal_form(normal_form_t *tmp) {
   safe_free(tmp);
 }
 
-
 /*
  * Copy the content of buffer b into f
  */
@@ -639,7 +553,6 @@ static void set_normal_form(normal_form_t *f, poly_buffer_t *b) {
   f->nterms = n;
   f->hash = hash_monarray(f->mono, n);
 }
-
 
 
 
@@ -712,7 +625,7 @@ static void extend_equality_queue(equality_queue_t *queue) {
  * Delete
  */
 static void delete_equality_queue(equality_queue_t *queue) {
-  safe_feee(queue->data);
+  safe_free(queue->data);
   safe_free(queue->parent);
   queue->data = NULL;
   queue->parent = NULL;
@@ -811,52 +724,558 @@ static void equality_queue_backtrack(equality_queue_t *queue) {
 }
 
 
+/*
+ * Call back: report that x and y are equal
+ * - aux is a pointer to am equality queue
+ * - x and y must be two variable indices in this table
+ */
+static void notify_equality(void *aux, int32_t x, int32_t y) {
+  equality_queue_t *queue;
+
+  queue = aux;
+  assert(1 <= x && x <= queue->nvars && 0 <= y && y <= queue->nvars);
+  printf("---> received equality: x%"PRId32" == x%"PRId32"\n", x, y);
+  fflush(stdout);
+
+  x = root_of_var(queue, x);
+  y = root_of_var(queue, y);
+  push_equality(queue, x, y);  
+}
+
 
 
 /*
- * Call back: report that x and y are equal
- * - aux is a pointer to a poly_table
- * - x and y must be two polynomial indices in this table
+ * TEST BENCH
  */
-static void notify_equality(void *aux, int32_t x, int32_t y) {
-  poly_table_t *table;
 
-  table = aux;
-  assert(1 <= x && x <= table->npolys && 0 <= y && y <= table->npolys);
-  printf("---> received equality: x%"PRId32" == x%"PRId32"\n", x, y);
-  fflush(stdout);
+/*
+ * Active polynomials:
+ * - a polynomial i is active if it was added to the offset manager
+ * - there are npolys active polynomials
+ * - for each i in 0 ... npolys - 1
+ * - id[i] = index in the relevant poly_table
+ * - norm[i] = normal form for the polynomial
+ * - also when we add activate polynomial k, we mark active[k] = true
+ */
+typedef struct active_poly_table_s {
+  uint32_t size;   // size of arrays id and norm
+  uint32_t npolys; // number of active polys stored
+  int32_t *id;
+  normal_form_t **norm;
+  uint8_t *active;
+} active_poly_table_t;
+
+#define MAX_ACTIVE_POLYS (UINT32_MAX/sizeof(normal_form_t *))
+
+// m = fixed size of the active poly table
+static void init_active_polys(active_poly_table_t *table, uint32_t n) {
+  uint32_t i;
+
+  assert(n <= MAX_ACTIVE_POLYS);
+
+  table->size = n;
+  table->npolys = 0;
+  table->id = (int32_t *) safe_malloc(n * sizeof(int32_t));
+  table->norm = (normal_form_t **) safe_malloc(n * sizeof(normal_form_t *));
+  table->active = (uint8_t *) safe_malloc(n * sizeof(uint8_t));
+
+  for (i=0; i<n; i++) {
+    table->active[i] = false;
+  }
+}
+
+static void delete_active_polys(active_poly_table_t *table) {
+  uint32_t i, n;
+
+  n = table->npolys;
+  for (i=0; i<n; i++) {
+    free_normal_form(table->norm[i]);
+  }
+  safe_free(table->id);
+  safe_free(table->norm);
+  safe_free(table->active);
+  table->id = NULL;
+  table->norm = NULL;
+  table->active = NULL;
+}
+
+static void add_active_poly(active_poly_table_t *table, poly_table_t *ptbl, int32_t i) {
+  polynomial_t *p;
+  normal_form_t *q;
+  uint32_t k, n;
+
+  assert(0 < i && i < ptbl->npolys && !table->active[i]);
+
+  p = ptbl->poly[i];
+  n = (p == NULL) ? 2 : (p->nterms + 1);
+  q = new_normal_form(n);
+  
+  k = table->npolys;
+  assert(k < table->size);
+  table->id[k] = i;
+  table->norm[k] = q;
+  table->npolys = k + 1;
+
+  table->active[i] = true;
+}
+
+static void remove_active_poly(active_poly_table_t *table) {
+  uint32_t k;
+  int32_t id;
+
+  assert(table->npolys > 0);
+
+  k = table->npolys - 1;
+  free_normal_form(table->norm[k]);
+  id = table->id[k];
+
+  assert(table->active[id]);
+  table->active[k] = false;
+
+  table->npolys = k;
 }
 
 
 /*
- * Operations on offset manager
- * - record_poly
- * - assert_eq
- * - propagate
- * - push
- * - pop
- * - reset
+ * Substitution queue
+ * - to undo/update the substitution, we keep track of variable
+ * - when we add x:= y + k to subst, we store x at the end of the variable queue
  */
+typedef struct subst_queue_s {
+  uint32_t size;
+  uint32_t top;
+  int32_t *data;
+} subst_queue_t;
 
+#define MAX_SQUEUE_SIZE (UINT32_MAX/sizeof(int32_t))
+
+// n = fixed size
+static void init_subst_queue(subst_queue_t *queue, uint32_t n) {
+  assert(n <= MAX_SQUEUE_SIZE);
+  queue->size = n;
+  queue->top = 0;
+  queue->data = (int32_t *) safe_malloc(n * sizeof(int32_t));
+}
+
+static void delete_subst_queue(subst_queue_t *queue) {
+  safe_free(queue->data);
+  queue->data = NULL;
+}
+
+static void subst_queue_push_var(subst_queue_t *queue, int32_t x) {
+  uint32_t i;
+
+  i = queue->top;
+  assert(i < queue->size);
+  queue->data[i] = x;
+  queue->top = i+1;
+}
+
+static int32_t subst_queue_pop_var(subst_queue_t *queue) {
+  assert(queue->top > 0);
+  queue->top --;
+  return queue->data[queue->top];
+}
+
+
+/*
+ * Queue of test operations
+ * - operations on offset_manager include
+ *   record_poly
+ *   assert_eq
+ *   propagate
+ *   push
+ *   pop
+ * - for testing we keep track of the sequence of operations
+ *   performed (except push)
+ * - desciptors for each operation:
+ *   record_poly(i): activate polynomial of index i
+ *  
+ */
+typedef enum operations {
+  RECORD_POLY,
+  ASSERT_EQ,
+  PROPAGATE,
+  PUSH,
+} operation_t;
+
+typedef struct op_desc_s {
+  operation_t tag;
+  union {
+    int32_t rec_id;        // argument of RECORD_POLY
+    offset_equality_t eq;  // argument of ASSERT_EQ
+  } arg;
+} op_desc_t;
+
+typedef struct op_stack_s {
+  uint32_t size;
+  uint32_t top;
+  op_desc_t *data;
+} op_stack_t;
+
+#define MAX_OP_STACK_SIZE (UINT32_MAX/sizeof(op_desc_t))
+
+static void init_op_stack(op_stack_t *stack) {
+  uint32_t n;
+
+  n = 100;
+  stack->size = n;
+  stack->top = 0;
+  stack->data = (op_desc_t *) safe_malloc(n * sizeof(op_stack_t));
+}
+
+static void extend_op_stack(op_stack_t *stack) {
+  uint32_t n;
+
+  n = stack->size * 2;
+  if (n > MAX_OP_STACK_SIZE) {
+    out_of_memory();
+  }
+  stack->size = n;
+  stack->data = (op_desc_t *) safe_realloc(stack->data, n * sizeof(op_stack_t));
+}
+
+static void delete_op_stack(op_stack_t *stack) {
+  safe_free(stack->data);
+  stack->data = NULL;
+}
+
+static void push_record_poly(op_stack_t *stack, int32_t id) {
+  uint32_t i;
+
+  i = stack->top;
+  if (i == stack->size) {
+    extend_op_stack(stack);
+  }
+  assert(i < stack->size);
+
+  stack->data[i].tag = RECORD_POLY;
+  stack->data[i].arg.rec_id = id;
+  stack->top = i+1;
+}
+
+static void push_assert_eq(op_stack_t *stack, int32_t x, int32_t y, int32_t offset) {
+  uint32_t i;
+
+  i = stack->top;
+  if (i == stack->size) {
+    extend_op_stack(stack);
+  }
+  assert(i < stack->size);
+
+  stack->data[i].tag = ASSERT_EQ;
+  stack->data[i].arg.eq.lhs = x;
+  stack->data[i].arg.eq.rhs = y;
+  stack->data[i].arg.eq.offset = offset;
+  stack->top = i+1;
+}
+
+static void push_op(op_stack_t *stack, operation_t op) {
+  uint32_t i;
+
+  assert(op == PROPAGATE || op == PUSH);
+
+  i = stack->top;
+  if (i == stack->size) {
+    extend_op_stack(stack);
+  }
+  assert(i < stack->size);
+
+  stack->data[i].tag = op;
+  stack->top = i+1;
+}
+
+static inline void push_propagate(op_stack_t *stack) {
+  push_op(stack, PROPAGATE);
+}
+
+static inline void push_push(op_stack_t *stack) {
+  push_op(stack, PUSH);
+}
+
+
+
+/*
+ * Test bench:
+ */
+typedef struct test_bench_s {
+  poly_table_t *ptable;
+  substitution_t subst;
+  subst_queue_t squeue;
+  active_poly_table_t act;
+  op_stack_t stack;
+  equality_queue_t equeue;
+  offset_manager_t manager; // under test
+
+  poly_buffer_t buffer; // for normalization
+} test_bench_t;
+
+static void init_test_bench(test_bench_t *bench, poly_table_t *ptable) {
+  uint32_t nv, np;
+
+  np = ptable->npolys;
+  nv = ptable->nvars;
+
+  bench->ptable = ptable;
+  init_substitution(&bench->subst, nv);
+  init_subst_queue(&bench->squeue, nv);
+  init_active_polys(&bench->act, np);
+  init_op_stack(&bench->stack);
+  init_equality_queue(&bench->equeue, nv);
+
+  init_offset_manager(&bench->manager, &bench->equeue, notify_equality);
+
+  init_poly_buffer(&bench->buffer);
+}
+
+static void delete_test_bench(test_bench_t *bench) {
+  delete_substitution(&bench->subst);
+  delete_subst_queue(&bench->squeue);
+  delete_active_polys(&bench->act);
+  delete_equality_queue(&bench->equeue);
+  delete_op_stack(&bench->stack);
+  delete_offset_manager(&bench->manager);
+  delete_poly_buffer(&bench->buffer);
+}
+
+
+/*
+ * Compute normal form of active poly k
+ */
+static void normalize(test_bench_t *bench, uint32_t k) {
+  poly_buffer_t *buffer;
+  int32_t idx;
+
+  assert(0 <= k && k < bench->act.npolys);
+  buffer = &bench->buffer;
+  idx = bench->act.id[k];
+
+  subst_poly_idx(&bench->subst, bench->ptable, buffer, idx);
+  set_normal_form(bench->act.norm[k], buffer);
+}
+
+static void normalize_all(test_bench_t *bench) {
+  uint32_t i, n;
+
+  n = bench->act.npolys;
+  for (i=0; i<n; i++) {
+    normalize(bench, i);
+  }
+}
+
+
+/*
+ * TEST OPERATIONS
+ */
+static void test_activate(test_bench_t *bench, int32_t id) {
+  polynomial_t *p;
+
+  add_active_poly(&bench->act, bench->ptable, id);
+  push_record_poly(&bench->stack, id);
+
+  p = bench->ptable->poly[id];
+  record_offset_poly(&bench->manager, id, id, p);
+}
+ 
+static void test_assert_eq(test_bench_t *bench, int32_t x, int32_t y, int32_t offset) {
+  offset_equality_t e;
+  rational_t q;
+
+  push_assert_eq(&bench->stack, x, y, offset);
+
+  e.lhs = x;
+  e.rhs = y;
+  e.offset = offset;
+  subst_eq(&bench->subst, &e);
+
+  if (e.lhs == e.rhs) {
+    if (e.offset != 0) {
+      // expected result = conflict
+    }
+  } else { 
+    if (e.lhs < 0) {
+      // convert 0 = y + offset into y := -offset
+      e.lhs = e.rhs;
+      e.rhs = -1;
+      e.offset = -e.offset;
+    }
+
+    // add substitution e.lhs := e.rhs + e.offset
+    add_subst(&bench->subst, e.lhs, e.rhs, e.offset);
+    subst_queue_push_var(&bench->squeue, e.lhs);
+
+    q_init(&q);
+    q_set32(&q, offset);
+    assert_offset_equality(&bench->manager, x, y, &q, e.lhs);
+    q_clear(&q);
+  }
+}
+
+static void test_propagate(test_bench_t *bench) {
+  push_propagate(&bench->stack);
+  offset_manager_propagate(&bench->manager);
+}
+
+static void test_push(test_bench_t *bench) {
+  push_push(&bench->stack);
+  push_mark(&bench->equeue);
+  offset_manager_push(&bench->manager);
+}
+
+
+
+/*
+ * PRINT FUNCTIONS
+ */
+static void print_mono(const char *prefix, rational_t *coeff, int32_t x, bool first) {
+  bool negative;
+  bool abs_one;
+
+  negative = q_is_neg(coeff);
+  if (negative) {
+    if (first) {
+      printf("- ");
+    } else {
+      printf(" - ");
+    }
+    abs_one = q_is_minus_one(coeff);
+  } else {
+    if (! first) {
+      printf(" + ");
+    }
+    abs_one = q_is_one(coeff);
+  }
+
+  if (x == const_idx) {
+    q_print_abs(stdout, coeff);
+  } else {
+    if (! abs_one) {
+      q_print_abs(stdout, coeff);
+      printf(" * ");
+    }
+    printf("%s%"PRId32, prefix, x);
+  }
+}
+
+static void print_poly(polynomial_t *p) {
+  uint32_t i, n;
+  bool first;
+
+  if (polynomial_is_zero(p)) {
+    printf("0");
+  } else {
+    n = p->nterms;
+    first = true;
+    for (i=0; i<n; i++) {
+      print_mono("x", &p->mono[i].coeff, p->mono[i].var, first);
+      first = false;
+    }
+  }
+}
+
+static void print_buffer(poly_buffer_t *b) {
+  monomial_t *mono;
+  uint32_t i, n;
+  bool first;
+
+  n = poly_buffer_nterms(b);
+  mono = poly_buffer_mono(b);
+  if (n == 0) {
+    printf("0");
+  } else {
+    first = true;
+    for (i=0; i<n; i++) {
+      print_mono("x", &mono[i].coeff, mono[i].var, first);
+      first = false;
+    }
+  }  
+}
+
+static void print_normal_form(normal_form_t *f) {
+  uint32_t i, n;
+  bool first;
+  
+  n = f->nterms;
+  if (n == 0) {
+    printf("0");
+  } else {
+    first = true;
+    for (i=0; i<n; i++) {
+      print_mono("x", &f->mono[i].coeff, f->mono[i].var, first);
+      first = false;
+    }
+  }
+}
+
+static void print_poly_table(poly_table_t *table) {
+  polynomial_t *p;
+  uint32_t i, n;
+
+  n = table->npolys;
+  for (i=0; i<n; i++) {
+    p = table->poly[i];
+    if (p != NULL) {
+      printf("  x%"PRIu32" := ", i);
+      print_poly(p);
+      printf("\n");
+    }
+  }
+}
+
+static void print_active_polys(test_bench_t *bench) {
+  polynomial_t *p;
+  uint32_t i, n;
+  int32_t idx;
+
+  n = bench->act.npolys;
+  for (i=0; i<n; i++) {
+    idx = bench->act.id[i];
+    printf("  act[%"PRIu32"]: x%"PRId32, i, idx);
+    p = bench->ptable->poly[idx];
+    if (p != NULL) {
+      printf(" = ");
+      print_poly(p);
+    } 
+    printf("\n");
+    printf("         ---> ");
+    print_normal_form(bench->act.norm[i]);
+    printf("\n");
+  }
+}
 
 
 /*
  * GLOBAL OBJECTS
  */
 static poly_table_t poly_table;
-static offset_manager_t offset_manager;
+static test_bench_t bench;
 
 int main(void) {
   init_rationals();
   init_poly_table(&poly_table, 100, 20);
   build_poly_table(&poly_table, 10, 100);
-  init_offset_manager(&offset_manager, &poly_table, notify_equality);
+  init_test_bench(&bench, &poly_table);
 
   printf("==== ALL POLYS ====\n");
   print_poly_table(&poly_table);
   printf("====\n");
 
-  delete_offset_manager(&offset_manager);
+  test_activate(&bench, 10);
+  test_activate(&bench, 2);
+  test_activate(&bench, 4);
+  test_propagate(&bench);
+  test_push(&bench);
+  test_activate(&bench, 43);
+  test_activate(&bench, 44);
+  test_propagate(&bench);
+  test_assert_eq(&bench, 4, 2, -12);
+  test_assert_eq(&bench, 4, 10, 0);
+  test_propagate(&bench);
+
+  normalize_all(&bench);
+  print_active_polys(&bench);
+
+  delete_test_bench(&bench);
   delete_poly_table(&poly_table);
   cleanup_rationals();
   
