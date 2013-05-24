9a8e1d203a9197db36907022aadf39da5db3ec11
Bruno Dutertre <bruno@csl.sri.com>
2013-05-24 21:37:19 +0000

    Non-inlined version of cputime
    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/private/yices2/branches/new-terms@2836 0c041c21-16df-db11-9388-003048836090

--- src/cputime.h
+++ src/cputime.h
@@ -5,34 +5,8 @@
 /*
  * get_cpu_time() returns CPU time (user + system time) used
  * by the process since its start.
- *
- * getrusage should work on all platforms we support,
- * except mingw
  */
-
-#ifndef MINGW
-
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <unistd.h>
-
-static inline double get_cpu_time(void) {
-  static struct rusage ru_buffer;
-  getrusage(RUSAGE_SELF, &ru_buffer);
-  return ru_buffer.ru_utime.tv_sec + ru_buffer.ru_stime.tv_sec 
-    + (ru_buffer.ru_utime.tv_usec + ru_buffer.ru_stime.tv_usec) * 1e-6;
-}
-
-#else
-
-#include <time.h>
-
-static inline double get_cpu_time(void) {
-  return ((double) clock())/CLOCKS_PER_SEC;
-}
-
-#endif
+extern double get_cpu_time(void);
 
 
 /*
