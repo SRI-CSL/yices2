c5508efd791e8c9dcac943afe9c044eea4eaf697
Bruno Dutertre <bruno@csl.sri.com>
2013-11-07 03:08:22 +0000

    More tests for finite-type enumeration
    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/private/yices2/branches/new-terms@3034 0c041c21-16df-db11-9388-003048836090

--- tests/test_concrete_values2.c
+++ tests/test_concrete_values2.c
@@ -59,7 +59,7 @@ static void init_base_types(void) {
   base[2] = new_scalar_type(&types, 3);    // scalar3 
   base[3] = new_scalar_type(&types, 1);    // scalar1
   base[4] = pair_type(base[0], base[2]);   // bool x scalar3
-  base[5] = pair_type(base[3], base[0]);  // scalar1 x bool
+  base[5] = pair_type(base[3], base[0]);   // scalar1 x bool
   base[6] = fun_type1(base[0], base[2]);   // [bool -> scalar3]
   base[7] = fun_type1(base[0], base[3]);   // [bool -> scalar1]
   base[8] = fun_type1(base[2], base[0]);   // [scalar3 -> bool]
@@ -90,18 +90,150 @@ static void test_enum_type(type_t tau) {
   printf("\n");
 }
 
-int main(void) {
+
+/*
+ * TEST1: all base types
+ */
+static void test_base_types(void) {
   uint32_t i;
 
-  init_type_table(&types, 10);
-  init_value_table(&vtbl, 0, &types);
-
-  init_base_types();
+  printf("*****************\n"
+	 "*   BASE TYPES  *\n"
+	 "*****************\n"
+	 "\n");
 
   for (i=0; i<NUM_BASE_TYPES; i++) {
     test_enum_type(base[i]);
   }
+}
+
+/*
+ * TEST2: pairs of base types
+ * - skip any pair type of cardinaly >= threshold
+ */
+static void test_pairs(uint32_t threshold) {
+  uint32_t i, j;
+  type_t tau;
+
+  printf("*****************\n"
+	 "*   PAIR TYPES  *\n"
+	 "*****************\n"
+	 "\n");
+
+  for (i=0; i<NUM_BASE_TYPES; i++) {
+    for (j=0; j<NUM_BASE_TYPES; j++) {
+      tau = pair_type(base[i], base[j]);
+      if (type_card(&types, tau) < threshold) {
+	test_enum_type(tau);
+      }
+    }
+  }
+}
+
+/*
+ * TEST3: triples
+ */
+static void test_triples(uint32_t threshold) {
+  uint32_t i, j, k;
+  type_t tau;
+
+  printf("*******************\n"
+	 "*   TRIPLE TYPES  *\n"
+	 "*******************\n"
+	 "\n");
+
+  for (i=0; i<NUM_BASE_TYPES; i++) {
+    for (j=0; j<NUM_BASE_TYPES; j++) {
+      for (k=0; k<NUM_BASE_TYPES; k++) {
+	tau = triple_type(base[i], base[j], base[k]);
+	if (type_card(&types, tau) < threshold) {
+	  test_enum_type(tau);
+	}
+      }
+    }
+  }
+}
+
+/*
+ * TEST4: unary functions
+ */
+static void test_unary_functions(uint32_t threshold) {
+  uint32_t i, j;
+  type_t tau;
+
+  printf("***************************\n"
+	 "*   UNARY FUNCTION TYPES  *\n"
+	 "***************************\n"
+	 "\n");
+
+  for (i=0; i<NUM_BASE_TYPES; i++) {
+    for (j=0; j<NUM_BASE_TYPES; j++) {
+      tau = fun_type1(base[i], base[j]);
+      if (type_card(&types, tau) < threshold) {
+	test_enum_type(tau);
+      }
+    }
+  }
+}
+
+
+/*
+ * TEST 5: binary functions
+ */
+static void test_bin_functions(uint32_t threshold) {
+  uint32_t i, j, k;
+  type_t tau;
+
+  printf("****************************\n"
+	 "*   BINARY FUNCTION TYPES  *\n"
+	 "****************************\n"
+	 "\n");
+
+  for (i=0; i<NUM_BASE_TYPES; i++) {
+    for (j=0; j<NUM_BASE_TYPES; j++) {
+      for (k=0; k<NUM_BASE_TYPES; k++) {
+	tau = fun_type2(base[i], base[j], base[k]);
+	if (type_card(&types, tau) < threshold) {
+	  test_enum_type(tau);
+	}
+      }
+    }
+  }
+}
+
+
+/*
+ * TEST6: (-> (-> (-> bool bool) bool) bool)
+ */
+static void test_deep_type(void) {
+  type_t b, tau;
+
+  b = bool_type(&types);
+  tau = fun_type1(b, b);  // (-> bool bool)     
+  tau = fun_type1(tau, b);   // (-> (-> bool bool) bool)
+  tau = fun_type1(tau, b);   // (-> (-> (-> bool bool) bool) bool)
   
+  printf("*****************************\n"
+	 "*   NESTED FUNCTION TYPE    *\n"
+	 "*****************************\n"
+	 "\n");
+
+  test_enum_type(tau);
+}
+
+
+int main(void) {
+  init_type_table(&types, 10);
+  init_value_table(&vtbl, 0, &types);
+
+  init_base_types();
+  test_base_types();
+  test_pairs(1000);
+  test_triples(1000);
+  test_unary_functions(1000);
+  test_bin_functions(1000);
+  test_deep_type();
+
   delete_value_table(&vtbl);
   delete_type_table(&types);
 
