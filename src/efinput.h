d04d83b6e160d8a58052fe698d1440f8d9e0b1d3
Bruno Dutertre <bruno@csl.sri.com>
2013-12-20 12:44:52 -0800

    Placeholder file efinput.h

---
+++ src/efinput.h
@@ -0,0 +1,20 @@
+/*
+ * INPUT TO THE EXISTS/FORALL SOLVER
+ */
+
+/*
+ * Data structure to store an exist/forall problem.
+ * The basic form is 
+ *
+ *    A(x) AND (FORALL y: B(y) => C(x, y))
+ *
+ * and the goal is to find x that satisfies this constraint.
+ *
+ * We take a set of input formulas [f1, ... f_n] and attempt to write
+ * them in the above form. We report an error if we can't do it.
+ */
+
+#ifndef __EFINPUT_H
+#define __EFINPUT_H
+
+#endif /* __EFINPUT_H */
