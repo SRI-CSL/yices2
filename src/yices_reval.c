09fd8fb30a2e3d0db8e0396e9050e2c3a21fbf2b
Bruno Dutertre <bruno@csl.sri.com>
2014-02-20 10:56:08 -0800

    Moved the 'dump-context' code out of 'yices_reval'.

--- src/yices_reval.c
+++ src/yices_reval.c
@@ -35,16 +35,7 @@
 #include "yices_exit_codes.h"
 
 // FOR DUMP
-#include "term_printer.h"
-#include "type_printer.h"
-#include "idl_fw_printer.h"
-#include "rdl_fw_printer.h"
-#include "simplex_printer.h"
-#include "gates_printer.h"
-#include "bvsolver_printer.h"
-#include "egraph_printer.h"
-#include "smt_core_printer.h"
-#include "context_printer.h"
+#include "dump_context.h"
 
 // FOR STATISTICS
 #include "simplex.h"
@@ -2295,141 +2286,12 @@ static void yices_showtimeout_cmd(void) {
 }
 
 
-/*
- * Dump: print all internal tables
- * + the egraph/core and theory solvers
- */
-static void dump_egraph(FILE *f, egraph_t *egraph) {
-  fprintf(f, "\n--- Egraph Variables ---\n");
-  print_egraph_terms(f, egraph);
-  fprintf(f, "\n--- Egraph Atoms ---\n");
-  print_egraph_atoms(f, egraph);
-#ifndef DEBUG
-  fprintf(f, "\n--- Egraph Classes ---\n");
-  print_egraph_root_classes(f, egraph);
-#endif
-}
-
-static void dump_idl_solver(FILE *f, idl_solver_t *idl) {
-  fprintf(f, "\n--- IDL Variables ---\n");
-  print_idl_var_table(f, idl);
-  fprintf(f, "\n--- IDL Atoms ---\n");
-  print_idl_atoms(f, idl);
-  fprintf(f, "\n--- IDL Constraints ---\n");
-  print_idl_axioms(f, idl);
-}
-
-static void dump_rdl_solver(FILE *f, rdl_solver_t *rdl) {
-  fprintf(f, "\n--- RDL Variables ---\n");
-  print_rdl_var_table(f, rdl);
-  fprintf(f, "\n--- RDL Atoms ---\n");
-  print_rdl_atoms(f, rdl);
-  fprintf(f, "\n--- RDL Constraints ---\n");
-  print_rdl_axioms(f, rdl);
-}
-
-static void dump_simplex_solver(FILE *f, simplex_solver_t *simplex) {
-  fprintf(f, "\n--- Simplex ---\n");
-#ifndef NDEBUG
-  fprintf(f, "status:         %s\n", status2string[simplex->core->status]);
-  print_simplex_flags(f, simplex);
-  fprintf(f, "\n");
-#endif
-  print_simplex_vars(f, simplex);
-#ifndef NDEBUG
-  print_simplex_saved_rows(f, simplex);
-#endif
-  print_simplex_atoms(f, simplex);
-  fprintf(f, "\n--- Tableau ---\n");
-  print_simplex_matrix(f, simplex);
-  fprintf(f, "---  Bounds ---\n");
-  print_simplex_bounds(f, simplex);
-  fprintf(f, "\n");
-}
-
-static void dump_bv_solver(FILE *f, bv_solver_t *solver) {
-#ifndef NDEBUG
-  fprintf(f, "\n--- Bitvector Partition ---\n");
-  print_bv_solver_partition(f, solver);
-#endif
-  fprintf(f, "\n--- Bitvector Variables ---\n");
-  print_bv_solver_vars(f, solver);
-  fprintf(f, "\n--- Bitvector Atoms ---\n");
-  print_bv_solver_atoms(f, solver);
-#ifndef NDEBUG
-  fprintf(f, "\n--- Bitvector Bounds ---\n");
-  print_bv_solver_bounds(f, solver);
-  fprintf(f, "\n--- DAG ---\n");
-  print_bv_solver_dag(f, solver);
-  if (solver->blaster != NULL) {
-    fprintf(f, "\n--- Gates ---\n");
-    print_gate_table(f, &solver->blaster->htbl);
-  }
-#endif
-  fprintf(f, "\n");
-}
-
-
 static void yices_dump_cmd(void) {
   if (efmode) {
     report_error("(dump-context) is not supported by the exists/forall solver");
   } else {
     assert(context != NULL);
-
-#ifndef NDEBUG
-    printf("--- Substitutions ---\n");
-    print_context_intern_subst(stdout, context);
-
-    printf("\n--- Internalization ---\n");
-    print_context_intern_mapping(stdout, context);
-
-    printf("\n--- Gates ---\n");
-    print_gate_table(stdout, &context->gate_manager.htbl);
-#endif
-
-    if (context_has_egraph(context)) {
-      dump_egraph(stdout, context->egraph);
-    }
-
-    if (context_has_arith_solver(context)) {
-      if (context_has_idl_solver(context)) {
-	dump_idl_solver(stdout, context->arith_solver);
-      } else if (context_has_rdl_solver(context)) {
-	dump_rdl_solver(stdout, context->arith_solver);
-      } else {
-	assert(context_has_simplex_solver(context));
-	dump_simplex_solver(stdout, context->arith_solver);
-      }
-    }
-
-    if (context_has_bv_solver(context)) {
-      dump_bv_solver(stdout, context->bv_solver);
-    }
-
-    /*
-     * If arch is still AUTO_IDL or AUTO_RDL,
-     * then flattening + simplification returned unsat
-     * but the core is not initialized
-     * so we can't print the clauses.
-     */
-    if (context->arch != CTX_ARCH_AUTO_IDL &&
-	context->arch != CTX_ARCH_AUTO_RDL) {
-      printf("--- Clauses ---\n");
-      print_clauses(stdout, context->core);
-      printf("\n");
-    }
-
-#if 0
-    printf("--- Auxiliary vectors ---\n");
-    print_context_subst_eqs(stdout, context);
-    print_context_top_eqs(stdout, context);
-    print_context_top_atoms(stdout, context);
-    print_context_top_formulas(stdout, context);
-    print_context_top_interns(stdout, context);
-    printf("\n");
-#endif
-
-    fflush(stdout);
+    dump_context(stdout, context);
   }
 }
 
