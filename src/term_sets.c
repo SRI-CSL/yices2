d8002d1d6bbed00289188746a8a11e5ae1e68ff4
Bruno Dutertre <bruno@csl.sri.com>
2014-04-16 19:12:14 -0700

    New module to simplify construction of sets of terms (for use by the variable elimination procedures).

---
+++ src/term_sets.c
@@ -0,0 +1,39 @@
+#include "memalloc.h"
+#include "term_sets.h"
+
+
+/*
+ * Initialize set:
+ * - initial content = all terms in a[0 ... n-1]
+ */
+void init_term_set(int_hset_t *set, uint32_t n, term_t *a) {
+  uint32_t i;
+
+  init_int_hset(set, 0);
+  for (i=0; i<n; i++) {
+    (void) int_hset_add(set, a[i]);
+  }
+}
+
+
+/*
+ * Build the set that contains terms a[0 ... n-1]
+ * - a may contain several times the same term.
+ * - duplicates are ignored
+ */
+int_hset_t *new_term_set(uint32_t n, term_t *a) {
+  int_hset_t *tmp;
+
+  tmp = (int_hset_t *) safe_malloc(sizeof(int_hset_t));
+  init_term_set(tmp, n, a);
+  return tmp;
+}
+
+/*
+ * Delete a set constructed by the previous function
+ */
+void free_term_set(int_hset_t *s) {
+  delete_int_hset(s);
+  safe_free(s);
+}
+
