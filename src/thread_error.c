3ffdfc6a1c37efa2c4fc1a2836c94a22e865df2c
Ian A. Mason <Ian.Mason@SRI.com>
2014-04-23 15:16:22 -0700

    Beefing up the tests on TLS error stuff.

--- src/thread_error.c
+++ src/thread_error.c
@@ -1,18 +1,29 @@
+#include <stdbool.h>
+
 #include "thread_error.h"
 
+#if HAS_TLS
+#define YICES_THREAD_LOCAL __thread
+#else
+#define YICES_THREAD_LOCAL static
+#endif
 
-__thread  int32_t _tl_errno;
+#include "yices_types.h"
+
+
+
+YICES_THREAD_LOCAL int32_t _tl_errno;
+
+YICES_THREAD_LOCAL bool error_initialized = false;
+YICES_THREAD_LOCAL error_report_t error;
+
 
 
 void set_tl_error(int32_t code){
-
   _tl_errno = code;
-
 }
 
 
 int32_t get_tl_error(void){
-
-
   return _tl_errno;
 }
