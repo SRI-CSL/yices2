5897aff3ceff88996703cc14fb6de011f9a3f776
Ian A. Mason <iam@csl.sri.com>
2014-04-23 17:30:07 -0700

    more debugging.

--- tests/test_thread_error.c
+++ tests/test_thread_error.c
@@ -91,11 +91,12 @@ yices_thread_result_t YICES_THREAD_ATTR test_thread(void* arg){
   thread_data_t* tdata = (thread_data_t *)arg;
   FILE* output = tdata->output;
 
+  fprintf(stderr, "%d starting tl test\n", tdata->id);
   test_tl_error(tdata, output);
-
+  fprintf(stderr, "%d starting yices error test\n", tdata->id);
   test_yices_error(tdata, output);
-  
-    return yices_thread_exit();
+  fprintf(stderr, "%d Done\n", tdata->id);
+  return yices_thread_exit();
 }
 
 
