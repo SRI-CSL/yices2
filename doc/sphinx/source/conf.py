90a24cf2f689f1fba03d4162b999fb66fd195998
Bruno Dutertre <bruno@csl.sri.com>
2015-02-02 23:30:36 -0800

    Fixed Copyright date

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Yices'
-copyright = u'2014, SRI International'
+copyright = u'2015, SRI International'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
