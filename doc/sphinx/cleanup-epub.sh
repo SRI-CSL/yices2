f2857ec9c01f90c8757352aab98ab182dfa13e0c
Bruno Dutertre <bruno@csl.sri.com>
2015-02-26 15:28:48 -0800

    Shell script to fix issues in the epub file that Sphinx generates.

---
+++ doc/sphinx/cleanup-epub.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# Cleanup the generated Yices.epub (in build/epub)
+#
+# Usage: cleanup-ebup.sh build-directory
+#
+# Remove 'role="..." >' from all html files
+# Copy the com.apple... file in META-INF
+# Rebuild Yices.epub
+#
+
+if [[ $# != 1 ]]; then
+  echo "Usage: $0 <build-directory>"
+  exit 1
+else
+  buildir=$1
+fi
+
+epubfile=Yices.epub
+apple=com.apple.ibooks.display-options.xml
+
+[[ -d $buildir ]] || { echo "Directory $buildir does not exit" ; exit 1 ; } 
+[[ -w $buildir/$epubfile ]] || { echo "File $buildir/$epubfile not found" ; exit 1 ; }
+[[ -r $apple ]] || { echo "File $apple not found" ; exit 1 ; }
+
+echo "Adding apple xml file"
+cp $apple $buildir/META-INF/ || { echo "Error: can't copy to $buildir/META-INF" ; exit 1 ; }
+
+cd $buildir 2> /dev/null || { echo "Failed to cd to $buildir" ; exit 1 ; }
+
+echo "Cleaning up the html files"
+for x in *.html ; do
+    sed -e 's/role=".*"//g' $x > $x.tmp
+    mv $x.tmp $x
+done
+
+zip -q $epubfile ./META-INF/$apple
+zip -q -f $epubfile
