6c1359d43d6c832da5864be6ba13990e2b2f1635
Bruno Dutertre <bruno@csl.sri.com>
2015-04-16 19:13:01 -0700

    Copyright edits and missing on one file

--- src/context/context_simplifier.h
+++ src/context/context_simplifier.h
@@ -1,4 +1,11 @@
 /*
+ * The Yices SMT Solver. Copyright 2015 SRI International.
+ *
+ * This program may only be used subject to the noncommercial end user
+ * license agreement which is downloadable along with this program.
+ */
+
+/*
  * UTiLITIES FOR SIMPLICATION AND PREPROCESSING
  */
 
