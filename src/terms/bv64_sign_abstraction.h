d2ec611eae30a8fafa90874002b7c8e298fb816d
Bruno Dutertre <bruno@csl.sri.com>
2015-10-30 15:39:44 -0700

    Started interval arithmetic for bitvectors

---
+++ src/terms/bv64_sign_abstraction.h
@@ -0,0 +1,150 @@
+/*
+ * The Yices SMT Solver. Copyright 2015 SRI International.
+ *
+ * This program may only be used subject to the noncommercial end user
+ * license agreement which is downloadable along with this program.
+ */
+
+/*
+ * INTERVAL ARITHMETIC FOR SIGNED BITVECTORS
+ */
+
+#ifndef __BV64_SIGN_ABSTRACTION_H
+#define __BV64_SIGN_ABSTRACTION_H
+
+/*
+ * Given an n-bit vector u = u[n-1] ... u[0], then we denote by
+ * bv2int(u) the 2s-complement interpretation of u as an integer.
+ *
+ * We then have:
+ *
+ *  bv2int(u) = Sum i=0 to n-2 of 2^i u[i] - 2^(n-1) u[n-1]
+ *
+ *  u[n-1] is the sign bit.
+ *
+ * Without more information we can only conclude that
+ *
+ *   -2^(n-1) <= bv2int(u) <= 2^(n-1) - 1
+ *
+ * If we have more information, we can often get more precise bounds.
+ * For example, if v has k bits and u is (sign-extend (n-k) v) then
+ * all bits u[n-1] ... u[n-k] are equal and we get
+ *
+ *   -2^(k-1) <= bv2int(u) <= 2^(k-1) - 1
+ *
+ * This means that we could represent u using (k+1) bits without loss
+ * of precision.
+ *
+ * In general, we want to reduce the size of arithmetic operation such
+ * as bvadd and bvmul, by using k bits instead of n when that doesn't
+ * lose precision. In general, give a polynomial p of n bits, we can
+ * compute the number of significant bits in p. If that's less than n,
+ * then we can convert p to (sign-extend k p') where p' is similar to
+ * p but with smaller-size operations.
+ *
+ * To compute the number of significant bits in a bitvector term u, we
+ * construct an abstraction:
+ *
+ *   alpha(u) = [k, s, l, h]
+ *
+ * where k+1 = number of significant bits in u
+ *         s = sign bit of u
+ *   l and h are two constants such that 0 <= l <= h <= 2^k -1
+ *
+ * This is intended to mean that bv2int(u) is equal to (v - 2^k s) for
+ * some k-bit vector v such that l <= v <= h.
+ *
+ * This gives us an interval for bv2int(u):
+ * - if s is zero, we get         l <= bv2int(u) <= h
+ * - if s is one,  we get (l - 2^k) <= bv2int(u) <= (h - 2^k)
+ * - otherwise,    we get (l - 2^k) <= bv2int(u) <= h           
+ *
+ */
+
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+/*
+ * Record to store the abstraction.
+ */
+typedef struct bv64_abs_s {
+  uint32_t nbits;  // number of significant bits - 1
+  int32_t  sign;   // sign bit
+  uint64_t low;    // lower bound
+  uint64_t high;   // upper bound
+} bv64_abs_t;
+
+/*
+ * For the sign bit we use the same conventions as in the smt core
+ * sat solver:
+ *  sign = null_literal (i.e., -1) means that the sign bit is unknown
+ *  sign = true_literal (i.e.,  0) means that the sign bit is '1'
+ *  sign = false_literal (i.e., 1) means that the sign bit is '0'
+ * Otherwise
+ *  sign = some non-negative integer l and we have not(l) = l^1
+ */
+enum {
+  sign_undef = -1,
+  sign_one = 0,  // i.e., true_literal
+  sign_zero = 1, // i.e., false_literal
+};
+
+static inline int32_t negate_sign(int32_t s) {
+  assert(s >= 0);
+  return s ^ 1;
+}
+
+
+
+/*
+ * Abstraction of a constant c.
+ * - n = number of bits in c
+ * - n must be positive and no more than 64
+ */
+extern void bv64_abs_constant(bv64_abs_t *a, uint64_t c, uint32_t n);
+
+
+/*
+ * Special cases: constants 0, +1, and -1
+ */
+extern void bv64_abs_zero(bv64_abs_t *a);
+extern void bv64_abs_one(bv64_abs_t *a);
+extern void bv64_abs_minus_one(bv64_abs_t *a);
+
+
+/*
+ * Abstraction of an array u of n bits.
+ * - n must be positive and no more thant 64
+ *
+ * We want this to work for both arrays of Boolean terms and arrays of
+ * literals.  Terms and literals use the same conventions:
+ * - they are represented by signed 32bit integers
+ * - the null term or literal is denoted by -1
+ * - negating a term or a literal corresponds to flipping the least
+ *   significant bit of the index.
+ * The only difference between terms and literals is the encoding of
+ * true and false: 
+ * - true_term = 2 and true_literal = 0
+ * - false_term = 3 and false_literal = 
+ *
+ * To deal with both cases, this function takes an extra parameter:
+ * zero, which must be the encoding of false.
+ */
+extern void bv64_abs_array(bv64_abs_t *a, int32_t zero, const int32_t *u, uint32_t n);
+
+
+/*
+ * Abstraction for binary operations: (a + b), (a - b), (a * b), (a ^ d)
+ * - all functions return false if the result requires more than 64bits
+ * - otherwise, the result is stored in a and the functions returns true.
+ */
+extern bool bv64_abs_add(bv64_abs_t *a, const bv64_abs_t *b);
+extern bool bv64_abs_sub(bv64_abs_t *a, const bv64_abs_t *b);
+extern bool bv64_abs_mul(bv64_abs_t *a, const bv64_abs_t *b);
+extern bool bv64_abs_power(bv64_abs_t *a, uint32_t d);
+
+
+#endif /* __BV64_SIGN_ABSTRACTION_H */
