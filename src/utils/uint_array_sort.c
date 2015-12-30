2f4ee783e0192ceb041401bab1c275c12f5cdfb3
Bruno Dutertre <bruno@csl.sri.com>
2015-12-30 01:05:15 -0800

    Sort functions for arrays of unsigned integers

---
+++ src/utils/uint_array_sort.c
@@ -0,0 +1,88 @@
+/*
+ * The Yices SMT Solver. Copyright 2015 SRI International.
+ *
+ * This program may only be used subject to the noncommercial end user
+ * license agreement which is downloadable along with this program.
+ */
+
+/*
+ * SORT INTEGER ARRAYS IN INCREASING ORDER
+ */
+
+/*
+ * This is the same code as in int_array_sort but for arrays of unsigned integers.
+ */
+
+#include "utils/uint_array_sort.h"
+#include "utils/prng.h"
+
+static void qsort_uint_array(uint32_t *a, uint32_t n);
+
+// insertion sort
+static void isort_uint_array(uint32_t *a, uint32_t n) {
+  uint32_t i, j;
+  uint32_t x, y;
+
+  for (i=1; i<n; i++) {
+    x = a[i];
+    j = 0;
+    while (a[j] < x) j ++;
+    while (j < i) {
+      y = a[j]; a[j] = x; x = y;
+      j ++;
+    }
+    a[j] = x;
+  }
+}
+
+static inline void sort_array(uint32_t *a, uint32_t n) {
+  if (n < 10) {
+    isort_uint_array(a, n);
+  } else {
+    qsort_uint_array(a, n);
+  }
+}
+
+// quick sort: requires n > 1
+static void qsort_uint_array(uint32_t *a, uint32_t n) {
+  uint32_t i, j;
+  uint32_t x, y;
+
+  // x = random pivot
+  i = random_uint(n);
+  x = a[i];
+
+  // swap x and a[0]
+  a[i] = a[0];
+  a[0] = x;
+
+  i = 0;
+  j = n;
+
+  do { j--; } while (a[j] > x);
+  do { i++; } while (i <= j && a[i] < x);
+
+  while (i < j) {
+    y = a[i]; a[i] = a[j]; a[j] = y;
+
+    do { j--; } while (a[j] > x);
+    do { i++; } while (a[i] < x);
+  }
+
+  // pivot goes into a[j]
+  a[0] = a[j];
+  a[j] = x;
+
+  // sort a[0...j-1] and a[j+1 .. n-1]
+  sort_array(a, j);
+  j++;
+  sort_array(a + j, n - j);
+}
+
+
+/*
+ * External call
+ */
+void uint_array_sort(uint32_t *a, uint32_t n) {
+  sort_array(a, n);
+}
---
+++ src/utils/uint_array_sort2.c
@@ -0,0 +1,85 @@
+/*
+ * The Yices SMT Solver. Copyright 2015 SRI International.
+ *
+ * This program may only be used subject to the noncommercial end user
+ * license agreement which is downloadable along with this program.
+ */
+
+/*
+ * SORT AN ARRAY OF UNSIGNED INTEGERS WITH A USER-SUPPLIED ORDERING
+ */
+
+#include "utils/uint_array_sort2.h"
+#include "utils/prng.h"
+
+static void qsort_uint_array2(uint32_t *a, uint32_t n, void *data, uint_cmp_fun_t cmp);
+
+
+// insertion sort
+static void isort_uint_array2(uint32_t *a, uint32_t n, void *data, uint_cmp_fun_t cmp) {
+  uint32_t i, j;
+  uint32_t x, y;
+
+  for (i=1; i<n; i++) {
+    x = a[i];
+    j = 0;
+    while (cmp(data, a[j], x)) j ++; // while (a[j] < x) j++;
+    while (j < i) {
+      y = a[j]; a[j] = x; x = y;
+      j ++;
+    }
+    a[j] = x;
+  }
+}
+
+static inline void sort_array2(uint32_t *a, uint32_t n, void *data, uint_cmp_fun_t cmp) {
+  if (n < 10) {
+    isort_uint_array2(a, n, data, cmp);
+  } else {
+    qsort_uint_array2(a, n, data, cmp);
+  }
+}
+
+// quick sort: requires n > 1
+static void qsort_uint_array2(uint32_t *a, uint32_t n, void *data, uint_cmp_fun_t cmp) {
+  uint32_t i, j;
+  uint32_t x, y;
+
+  // x = random pivot
+  i = random_uint(n);
+  x = a[i];
+
+  // swap x and a[0]
+  a[i] = a[0];
+  a[0] = x;
+
+  i = 0;
+  j = n;
+
+  do { j--; } while (cmp(data, x, a[j]));            // do { j--; } while (x < a[j]);
+  do { i++; } while (i <= j && cmp(data, a[i], x));  // do { i++; } while (a[i] < x);
+
+  while (i < j) {
+    y = a[i]; a[i] = a[j]; a[j] = y;
+
+    do { j--; } while (cmp(data, x, a[j]));  // do { j--; } while (x < a[j]);
+    do { i++; } while (cmp(data, a[i], x));  // do { j++; } while (a[i] < x);
+  }
+
+  // the pivot goes into a[j]
+  a[0] = a[j];
+  a[j] = x;
+
+  // sort a[0...j-1] and a[j+1 .. n-1]
+  sort_array2(a, j, data, cmp);
+  j++;
+  sort_array2(a + j, n - j, data, cmp);
+}
+
+
+/*
+ * External call
+ */
+void uint_array_sort2(uint32_t *a, uint32_t n, void *data, uint_cmp_fun_t cmp) {
+  sort_array2(a, n, data, cmp);
+}
