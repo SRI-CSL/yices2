ba1fadf3109c2a3ae50ab30eb7750e997edcc867
Bruno Dutertre <bruno@csl.sri.com>
2016-01-04 01:51:18 -0800

    Added stats flag + show propagation speed

--- src/frontend/yices_sat.c
+++ src/frontend/yices_sat.c
@@ -268,12 +268,14 @@ static int build_instance(char *filename) {
  * - model = true for produce model (if SAT)
  * - seed_given = true if a seed is given on the command line
  *   seed_value = value of the seed
+ * - stats = true for showing statistics at the end
  */
 static char *input_filename = NULL;
 static bool verbose;
 static bool model;
 static bool seed_given;
 static uint32_t seed_value;
+static bool stats;
 
 enum {
   version_flag,
@@ -281,9 +283,10 @@ enum {
   verbose_flag,
   model_flag,
   seed_opt,
+  stats_flag,
 };
 
-#define NUM_OPTIONS (seed_opt+1)
+#define NUM_OPTIONS (stats_flag+1)
 
 static option_desc_t options[NUM_OPTIONS] = {
   { "version", 'V', FLAG_OPTION, version_flag },
@@ -291,6 +294,7 @@ static option_desc_t options[NUM_OPTIONS] = {
   { "verbose", 'v', FLAG_OPTION, verbose_flag },
   { "model", 'm', FLAG_OPTION, model_flag },
   { "seed", 's', MANDATORY_INT, seed_opt },
+  { "stats", '\0', FLAG_OPTION, stats_flag },
 };
 
 
@@ -316,6 +320,7 @@ static void print_help(char *progname) {
          "   --help, -h           Print this message and exit\n"
          "   --model, -m          Show a model if the problem is satisfiable\n"
          "   --verbose, -v        Print statistics during the search\n"
+	 "   --stats              Print statistics at the end of the search\n"
          "\n"
          "For bug reporting and other information, please see http://yices.csl.sri.com/\n");
   fflush(stdout);
@@ -343,6 +348,7 @@ static void parse_command_line(int argc, char *argv[]) {
   model = false;
   verbose = false;
   seed_given = false;
+  stats = false;
 
   init_cmdline_parser(&parser, options, NUM_OPTIONS, argv, argc);
 
@@ -384,6 +390,10 @@ static void parse_command_line(int argc, char *argv[]) {
 	seed_given = true;
 	seed_value = elem.i_value;
 	break;
+
+      case stats_flag:
+        stats = true;
+        break;
       }
       break;
 
@@ -426,6 +436,7 @@ static void show_stats(solver_stats_t *stat) {
   fprintf(stderr, "deleted pb. clauses     : %"PRIu64"\n", stat->prob_clauses_deleted);
   fprintf(stderr, "deleted learned clauses : %"PRIu64"\n", stat->learned_clauses_deleted);
   fprintf(stderr, "deleted binary clauses  : %"PRIu64"\n", stat->bin_clauses_deleted);
+  fprintf(stderr, "\n");
 }
 
 
@@ -433,19 +444,22 @@ static void print_results(void) {
   solver_stats_t *stat;
   int resu;
   double mem_used;
+  double speed;
 
   search_time = get_cpu_time() - construction_time;
 
   stat = &solver.stats;
   resu = solver.status;
 
-  if (verbose) {
+  if (verbose || stats) {
     show_stats(stat);
     fprintf(stderr, "Search time             : %.4f s\n", search_time);
     mem_used = mem_size() / (1024 * 1024);
     if (mem_used > 0) {
       fprintf(stderr, "Memory used             : %.2f MB\n", mem_used);
     }
+    speed = stat->propagations/search_time;
+    fprintf(stderr, "Speed                   : %.2f prop/s\n", speed);
     fprintf(stderr, "\n\n");
   }
 
