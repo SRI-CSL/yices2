056fc19471713aaeaeb2ceed826c808c5ae14da0
Bruno Dutertre <bruno@csl.sri.com>
2016-04-16 10:59:16 -0700

    Removed debugging stuff

--- src/context/context_solver.c
+++ src/context/context_solver.c
@@ -40,7 +40,7 @@ static void trace_stats(smt_core_t *core, const char *when, uint32_t level) {
 	  num_binary_clauses(core), num_prob_clauses(core), num_prob_literals(core),
 	  num_learned_clauses(core), num_learned_literals(core),
 	  (double) num_learned_literals(core)/num_learned_clauses(core));
-#if 1
+#if 0
   fprintf(stderr,
 	  "(%-10s %8"PRIu64" %10"PRIu64" %8"PRIu64" %8"PRIu32" %8"PRIu32" %8"PRIu64" %8"PRIu32" %8"PRIu64" %7.1f)\n",
 	  when, core->stats.conflicts, core->stats.decisions, core->stats.random_decisions,
