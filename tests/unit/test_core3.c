b8af368daa30744c536a8f426d91d23fd1889c5b
Bruno Dutertre <bruno@csl.sri.com>
2017-05-04 11:21:41 -0700

    Fixed unit tests

--- tests/unit/test_core3.c
+++ tests/unit/test_core3.c
@@ -162,7 +162,7 @@ static void sat_search(smt_core_t *core, uint32_t conflict_bound, uint32_t *redu
   max_conflicts = num_conflicts(core) + conflict_bound;
   r_threshold = *reduce_threshold;
 
-  smt_process(core);
+  smt_process(core, UINT32_MAX);
   while (smt_status(core) == STATUS_SEARCHING && num_conflicts(core) <= max_conflicts) {
     // reduce heuristic
     if (num_learned_clauses(core) >= r_threshold) {
@@ -179,7 +179,7 @@ static void sat_search(smt_core_t *core, uint32_t conflict_bound, uint32_t *redu
     }
     // propagation
     decide_literal(core, l);
-    smt_process(core);
+    smt_process(core, UINT32_MAX);
   }
 
   *reduce_threshold = r_threshold;
@@ -239,7 +239,7 @@ static void sat_solve(smt_core_t *core, core_param_t *params, bool verbose) {
 
   // initialize then do a propagation + simplification step.
   start_search(core);
-  smt_process(core);
+  smt_process(core, UINT32_MAX);
   if (verbose) {
     show_progress(core, d_threshold, reduce_threshold, true);
   }
