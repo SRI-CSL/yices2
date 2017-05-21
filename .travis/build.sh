98f155142a73bde8e9a4da48ce447214871ba415
Ian A. Mason <iam@csl.sri.com>
2017-05-21 14:00:04 -0700

    The search for libpoly V

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,13 +2,14 @@
 # Make sure we exit if there is a failure
 set -e
 
-#build and install libpoly (dejan when will this be a ubuntu package?)
+#build and install libpoly (Dejan when will this be a ubuntu package?)
 git clone https://github.com/SRI-CSL/libpoly.git
 cd libpoly/build
 cmake ..
 make
 sudo make install
 
+#magic for solving mystery
 sudo updatedb
 locate libpoly.so.0
 sudo ldconfig -n /usr/local/lib/
@@ -24,6 +25,8 @@ sudo make install
 which yices-smt
 ldd /usr/local/bin/yices-smt
 
+#FIXME: this is needed for yices2 to find libpoly.so.0. Seems like there
+#should be a better way, no? Dejan?
 export LD_LIBRARY_PATH=/usr/local/lib/:${LD_LIBRARY_PATH}
 
 yices-smt --version
