70939f7ae0cca45de8d136783285e7d3ae491768
Bruno Dutertre <bruno@csl.sri.com>
2017-10-16 10:42:17 -0700

    Missed other occurrence of the version number

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -120,10 +120,10 @@ html_theme_options = {}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Yices 2.6 Documentation"
+html_title = "Yices 2.5 Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Yices 2.6"
+html_short_title = "Yices 2.5"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
