d443da0be14a8ee696e74bc0993b509a6c90c908
Bruno Dutertre <bruno@csl.sri.com>
2018-04-12 17:02:11 -0700

    Improvements to the context simplifications.
    1) detect when a disjunct is trivially true
    
    When processing (or a_1 ... a_n), we check whether one of a_i
    is known to be true. In such a case, the disjunct is true and
    we skip internalization of the other a_j's.
    
    2) better estimate of path lengths for difference-logic
    
    The upper bound on the max length of a simple path is less
    crude. So we should be able to use the Floyd-Warshall IDL
    solver on more instances.
    
    3) Other small fixes: typo in Makefile.

--- src/context/context_types.h
+++ src/context/context_types.h
@@ -38,6 +38,7 @@
 #include "solvers/cdcl/smt_core.h"
 #include "solvers/egraph/egraph.h"
 #include "terms/conditionals.h"
+#include "terms/int_rational_hash_maps.h"
 #include "terms/poly_buffer.h"
 #include "terms/terms.h"
 #include "utils/int_bv_sets.h"
@@ -602,13 +603,13 @@ typedef struct bv_interface_s {
  * or a specialized Floyd-Warshall solver. The decision is
  * based on the following parameters:
  * - density = number of atoms / number of variables
- * - sum_const = sum of the absolute values of all constants in the
- *   difference logic atoms
+ * - path_bound = an upper bound on the length of any simple
+ *                path between two variables
  * - num_eqs = number of equalities (among all atoms)
  * dl_data stores the relevant data
  */
 typedef struct dl_data_s {
-  rational_t sum_const;
+  rational_t path_bound;
   uint32_t num_vars;
   uint32_t num_atoms;
   uint32_t num_eqs;
@@ -684,6 +685,7 @@ struct context_s {
   mark_vector_t *marks;
   int_bvset_t *cache;
   int_hset_t *small_cache;
+  int_rat_hmap_t *edge_map;
   pmap2_t *eq_cache;
   divmod_tbl_t *divmod_table;
   bfs_explorer_t *explorer;
