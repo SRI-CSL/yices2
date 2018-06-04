33e4f9d15dc7c277fbcc6a85c970d4ce4bf4fb08
Bruno Dutertre <bruno@csl.sri.com>
2018-06-04 16:38:41 -0700

    Fixed a compiler warning.
    Also removed print when we skip named assertions. This is printed
    only when verbosity >= 20.

--- src/frontend/smt2/smt2_commands.c
+++ src/frontend/smt2/smt2_commands.c
@@ -5396,7 +5396,7 @@ void smt2_assert(term_t t, bool special) {
 	  if (!special || !g->produce_unsat_cores) {
 	    add_delayed_assertion(g, t);
 	  } else {
-	    printf("--> skipping named assertion");
+	    trace_printf(g->tracer, 20, "(skipping named assertion)\n");
 	  }
 	  report_success();
 	}
@@ -5404,7 +5404,7 @@ void smt2_assert(term_t t, bool special) {
 	if (!special || !g->produce_unsat_cores) {
 	  add_assertion(g, t);
 	} else {
-	  printf("--> skipping named assertion");
+	  trace_printf(g->tracer, 20, "(skipping named assertion)\n");
 	}
       }
     } else {
