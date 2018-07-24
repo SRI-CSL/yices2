80087e2a07285b71dd914291f3b58e4c082b04b0
Dejan Jovanovic <dejan.jovanovic@sri.com>
2018-07-24 15:14:25 -0400

    missing SGE case

--- src/mcsat/bv/bv_utils.h
+++ src/mcsat/bv/bv_utils.h
@@ -205,6 +205,15 @@ void bv_term_compute_value(term_table_t* terms, term_t t, bvconstant_t** childre
       }
       break;
     }
+    case BV_SGE_ATOM: {
+      bool values_sge = bvconst_sge(children_values[0]->data, children_values[1]->data, bitsize);
+      if (values_sge) {
+        bvconst_set_bit(out_value->data, 0);
+      } else {
+        bvconst_clr_bit(out_value->data, 0);
+      }
+      break;
+    }
     case BV_DIV:
       bvconst_udiv2z(out_value->data, bitsize, children_values[0]->data, children_values[1]->data);
       break;
