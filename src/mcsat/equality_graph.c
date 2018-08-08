eddb175d3b71421cd48cca5001bf4428151c2362
Dejan Jovanovic <dejan.jovanovic@sri.com>
2018-08-08 14:11:52 -0400

    basic frame for equality

---
+++ src/mcsat/equality_graph.c
@@ -0,0 +1,164 @@
+/*
+ * This file is part of the Yices SMT Solver.
+ * Copyright (C) 2017 SRI International.
+ *
+ * Yices is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Yices is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yices.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "equality_graph.h"
+#include "utils/memalloc.h"
+
+#include <assert.h>
+
+void equality_graph_construct(equality_graph_t* eq, plugin_context_t* ctx, const char* name) {
+  eq->ctx = ctx;
+
+  eq->capacity = 0;
+  eq->size = 0;
+  eq->graph_nodes = 0;
+
+  eq->name = name;
+
+  init_int_hmap(&eq->term_to_id, 0);
+  init_value_hmap(&eq->value_to_id, 0);
+  init_ivector(&eq->terms_list, 0);
+  init_pvector(&eq->values_list, 0);
+
+  scope_holder_construct(&eq->scope_holder);
+}
+
+void equality_graph_destruct(equality_graph_t* eq) {
+
+  safe_free(eq->graph_nodes);
+
+  delete_int_hmap(&eq->term_to_id);
+  delete_value_hmap(&eq->value_to_id);
+  delete_ivector(&eq->terms_list);
+  delete_pvector(&eq->values_list);
+}
+
+
+// Default initial size and max size
+#define DEFAULT_GRAPH_SIZE 10
+#define MAX_GRAPH_SIZE (UINT32_MAX/sizeof(equality_graph_node_t))
+
+static
+equality_graph_node_t* equality_graph_new_node(equality_graph_t* eq) {
+  uint32_t n = eq->size;
+
+  // Check if we need to resize
+  if (n == eq->capacity) {
+    // Compute new size
+    if (n == 0) {
+      n = DEFAULT_GRAPH_SIZE;
+    } else {
+      n ++;
+      n += n >> 1;
+      if (n >= MAX_GRAPH_SIZE) {
+        out_of_memory();
+      }
+    }
+    // Resize
+    eq->graph_nodes = (equality_graph_node_t*) safe_realloc(eq->graph_nodes, n * sizeof(equality_graph_node_t));
+    eq->capacity = n;
+  }
+
+  // Return the new element
+  return &eq->graph_nodes[eq->size ++];
+}
+
+bool equality_graph_add_term(equality_graph_t* eq, term_t t) {
+
+  // New id of the node
+  uint32_t id = eq->size;
+  uint32_t index = eq->terms_list.size;
+
+  // Check if already there
+  int_hmap_pair_t* find = int_hmap_get(&eq->term_to_id, t);
+  if (find->val < 0) {
+    find->val = id;
+    ivector_push(&eq->terms_list, t);
+  } else {
+    return false;
+  }
+
+  // Setup the new node
+  equality_graph_node_t* node = equality_graph_new_node(eq);
+  node->find = id;
+  node->next = id;
+  node->index = index;
+  node->is_constant = is_const_term(eq->ctx->terms, t);
+
+  assert(eq->terms_list.size + eq->values_list.size == eq->size);
+
+  // Added, done
+  return true;
+}
+
+bool equality_graph_add_value(equality_graph_t* eq, const mcsat_value_t* v) {
+
+  // New id of the node
+  uint32_t id = eq->size;
+  uint32_t index = eq->values_list.size;
+
+  // Check if already there
+  value_hmap_pair_t* find = value_hmap_get(&eq->value_to_id, v);
+  if (find->val < 0) {
+    find->val = id;
+    pvector_push(&eq->values_list, (void*) v);
+  } else {
+    return false;
+  }
+
+  // Setup the new node
+  equality_graph_node_t* node = equality_graph_new_node(eq);
+  node->find = id;
+  node->next = id;
+  node->index = index;
+  node->is_constant = true;
+
+  assert(eq->terms_list.size + eq->values_list.size == eq->size);
+
+  // Added, done
+  return true;
+}
+
+bool equality_graph_has_term(const equality_graph_t* eq, variable_t t) {
+  return int_hmap_find((const int_hmap_t*) &eq->term_to_id, t) != NULL;
+}
+
+bool equality_graph_has_value(const equality_graph_t* eq, const mcsat_value_t* v) {
+  return value_hmap_find(&eq->value_to_id, v) != NULL;
+}
+
+void equality_graph_push(equality_graph_t* eq) {
+  scope_holder_push(&eq->scope_holder,
+      &eq->terms_list.size,
+      &eq->values_list.size,
+      NULL
+  );
+}
+
+void equality_graph_pop(equality_graph_t* eq) {
+  uint32_t term_list_size, value_list_size;
+
+  scope_holder_pop(&eq->scope_holder,
+      &term_list_size,
+      &value_list_size,
+      NULL
+  );
+
+  // TODO: actually remove data
+  assert(false);
+}
---
+++ src/mcsat/equality_graph.h
@@ -0,0 +1,124 @@
+/*
+ * This file is part of the Yices SMT Solver.
+ * Copyright (C) 2017 SRI International.
+ *
+ * Yices is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Yices is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yices.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <mcsat/utils/value_hash_map.h>
+#include <stdbool.h>
+
+#include "plugin.h"
+#include "value.h"
+
+#include "utils/scope_holder.h"
+#include "utils/int_vectors.h"
+#include "mcsat/utils/value_hash_map.h"
+
+/** Nodes in the graph have IDs, this is the type */
+typedef uint32_t equality_graph_node_id;
+
+/** Node in the equality graph */
+typedef struct equality_graph_node_s {
+  /** Id of the representative */
+  equality_graph_node_id find;
+  /** Next node in the class */
+  equality_graph_node_id next;
+  /** Index of the term (positive) or value (negative) */
+  int32_t index;
+  /** Is it a constant (values are constants, but so are some terms) */
+  bool is_constant;
+} equality_graph_node_t;
+
+/**
+ * Traditional functionality:
+ * - Add terms to the term database
+ * - Assert terms are equal/disequal (merge)
+ * 
+ * We use terms as the representation for terms in order to accomodate 
+ * for plugins adding terms that are not in the variable database (e.g., 
+ * bit-vector slicing).
+ * 
+ * MCSAT specific functionality:
+ * - Assert term is equal/disequal to value
+ * - Values are managed outside
+ * - Garbage collection: remove unmarked terms
+ * - Push/pop:
+ *   - removes assertions 
+ *   - removes values
+ *   - remove terms
+ */
+typedef struct equality_graph_s {
+
+  /** Map from terms to id */
+  int_hmap_t term_to_id;
+
+  /** Map from values to id */
+  value_hmap_t value_to_id;
+
+  /** List of the terms added in order */
+  ivector_t terms_list;
+
+  /** List of MCSAT values added in order */
+  pvector_t values_list;
+
+  /** Scope holder for push/pop */
+  scope_holder_t scope_holder;
+
+  /** Name of the equality graph */
+  const char* name;
+
+  /** 
+   * The data holding the nodes in the graph:
+   * - terms are in graph_nodes[0 .. ] */
+  equality_graph_node_t* graph_nodes;
+
+  /** Size of the graph nodes */
+  uint32_t size;
+
+  /** Capacity of the graph nodes */
+  uint32_t capacity;
+
+  /** The plugin context */
+  plugin_context_t* ctx;
+
+} equality_graph_t;
+
+
+/** Construct a new named equality graph. */
+void equality_graph_construct(equality_graph_t* eq, plugin_context_t* ctx, const char* name);
+
+/** Destruct the graph */
+void equality_graph_destruct(equality_graph_t* eq);
+
+/** Add the term to the database (returns true if newly added). */
+bool equality_graph_add_term(equality_graph_t* eq, term_t t);
+
+/** Add the value to the database (value managed outside and must survive until pop). */
+bool equality_graph_add_value(equality_graph_t* eq, const mcsat_value_t* v);
+
+/** Is the term already in the graph */
+bool equality_graph_has_term(const equality_graph_t* eq, variable_t t);
+
+/** Is the value already in the graph */
+bool equality_graph_has_value(const equality_graph_t* eq, const mcsat_value_t* v);
+
+/** Push the context */
+void equality_graph_push(equality_graph_t* eq);
+
+/** Pop the context */
+void equality_graph_pop(equality_graph_t* eq);
+
