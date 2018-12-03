674b8569cc9d41c7f006aff29420076a42b15f5d
Dejan Jovanovic <dejan.jovanovic@sri.com>
2018-12-03 16:18:48 -0500

    some refactoring

--- src/mcsat/bv/bv_explainer.c
+++ src/mcsat/bv/bv_explainer.c
@@ -12,10 +12,28 @@
 
 #include <inttypes.h>
 
+static
+const char* subtheory_to_string(bv_subtheory_t th) {
+  switch (th) {
+    case BV_TH_EQ: return "equality";
+    case BV_TH_EQ_EXT_CON: return "extract/concat";
+    case BV_TH_BITWISE: return "bitwise";
+    case BV_TH_SHIFT: return "shifts";
+    case BV_TH_ARITH_CMP: return "compare";
+    case BV_TH_ARITH: return "arithmetic";
+    case BV_TH_FULL: return "full";
+    default:
+      assert(false);
+  }
+  return "unknown";
+}
+
 void bv_explainer_construct(bv_explainer_t* exp, const plugin_context_t* ctx, watch_list_manager_t* wlm) {
   exp->ctx = ctx;
   exp->tm = &ctx->var_db->tm;
   exp->wlm = wlm;
+
+  init_int_hset(&exp->visited_cache, 0);
 
   exp->stats.th_eq = statistics_new_int(exp->ctx->stats, "mcsat::bv::conflicts_eq");
   exp->stats.th_eq_ext_con = statistics_new_int(exp->ctx->stats, "mcsat::bv::conflict_eq_ext_con");
@@ -23,9 +41,168 @@ void bv_explainer_construct(bv_explainer_t* exp, const plugin_context_t* ctx, wa
 }
 
 void bv_explainer_destruct(bv_explainer_t* exp) {
+  delete_int_hset(&exp->visited_cache);
 }
 
-bv_subtheory_t bv_explainer_get_subtheory(const bv_explainer_t* exp, const ivector_t* conflict) {
+static
+void bv_explainer_get_subtheories(bv_explainer_t* exp, term_t t, int* subtheory_count) {
+
+  assert(is_pos_term(t));
+
+  // SKip if already visited
+  if (int_hset_member(&exp->visited_cache, t)) {
+    return;
+  }
+
+  // The term table
+  term_table_t* terms = exp->ctx->terms;
+
+  term_kind_t kind = term_kind(terms, t);
+  switch (kind) {
+  case CONSTANT_TERM:
+  case BV_CONSTANT:
+  case BV64_CONSTANT:
+    // Nothing to do really
+    break;
+  case EQ_TERM: {
+    // Equality. Belongs to all sub-theories
+    composite_term_t* atom_comp = composite_term_desc(terms, t);
+    assert(atom_comp->arity == 2);
+    term_t t0 = atom_comp->arg[0], t0_pos = unsigned_term(t0);
+    if (t0 != t0_pos) subtheory_count[BV_TH_BITWISE] ++;
+    term_t t1 = atom_comp->arg[1], t1_pos = unsigned_term(t1);
+    if (t1 != t1_pos) subtheory_count[BV_TH_BITWISE] ++;
+    bv_explainer_get_subtheories(exp, t0_pos, subtheory_count);
+    bv_explainer_get_subtheories(exp, t1_pos, subtheory_count);
+    subtheory_count[BV_TH_EQ] ++;
+    break;
+  }
+  case BV_EQ_ATOM: {
+    composite_term_t* atom_comp = composite_term_desc(terms, t);
+    assert(atom_comp->arity == 2);
+    bv_explainer_get_subtheories(exp, atom_comp->arg[0], subtheory_count);
+    bv_explainer_get_subtheories(exp, atom_comp->arg[1], subtheory_count);
+    subtheory_count[BV_TH_EQ] ++;
+    break;
+  }
+  case BV_GE_ATOM:
+  case BV_SGE_ATOM: {
+    composite_term_t* atom_comp = composite_term_desc(terms, t);
+    assert(atom_comp->arity == 2);
+    bv_explainer_get_subtheories(exp, atom_comp->arg[0], subtheory_count);
+    bv_explainer_get_subtheories(exp, atom_comp->arg[1], subtheory_count);
+    subtheory_count[BV_TH_ARITH_CMP] ++;
+    break;
+  }
+  case BV_ARRAY: {
+    composite_term_t* concat_desc = bvarray_term_desc(terms, t);
+    for (uint32_t i = 0; i < concat_desc->arity; ++ i) {
+      term_t t_i = concat_desc->arg[i];
+      term_t t_i_pos = unsigned_term(t_i);
+      if (t_i != t_i_pos) subtheory_count[BV_TH_BITWISE] ++;
+      bv_explainer_get_subtheories(exp, t_i_pos, subtheory_count);
+    }
+    subtheory_count[BV_TH_EQ_EXT_CON] ++;
+    break;
+  }
+  case OR_TERM: {
+    composite_term_t* t_comp = or_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_comp->arity; ++ i) {
+      term_t t_i = t_comp->arg[i];
+      term_t t_i_pos = unsigned_term(t_i);
+      bv_explainer_get_subtheories(exp, t_i_pos, subtheory_count);
+    }
+    subtheory_count[BV_TH_BITWISE] ++;
+    break;
+  }
+  case BV_DIV:
+  case BV_REM:
+  case BV_SDIV:
+  case BV_SREM:
+  case BV_SMOD:{
+    composite_term_t* t_comp = composite_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_comp->arity; ++ i) {
+      bv_explainer_get_subtheories(exp, t_comp->arg[i], subtheory_count);
+    }
+    subtheory_count[BV_TH_ARITH] ++;
+    break;
+  }
+  case BV_SHL:
+  case BV_LSHR:
+  case BV_ASHR: {
+    composite_term_t* t_comp = composite_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_comp->arity; ++ i) {
+      bv_explainer_get_subtheories(exp, t_comp->arg[i], subtheory_count);
+    }
+    subtheory_count[BV_TH_SHIFT] ++;
+    break;
+  }
+  case BIT_TERM:
+    bv_explainer_get_subtheories(exp, bit_term_arg(terms, t), subtheory_count);
+    subtheory_count[BV_TH_EQ_EXT_CON] ++;
+    break;
+  case BV_POLY: {
+    bvpoly_t* t_poly = bvpoly_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_poly->nterms; ++ i) {
+      if (t_poly->mono[i].var == const_idx) continue;
+      bv_explainer_get_subtheories(exp, t_poly->mono[i].var, subtheory_count);
+    }
+    subtheory_count[BV_TH_ARITH] ++;
+    break;
+  }
+  case BV64_POLY: {
+    bvpoly64_t* t_poly = bvpoly64_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_poly->nterms; ++ i) {
+      if (t_poly->mono[i].var == const_idx) continue;
+      bv_explainer_get_subtheories(exp, t_poly->mono[i].var, subtheory_count);
+    }
+    subtheory_count[BV_TH_ARITH] ++;
+    break;
+  }
+  case POWER_PRODUCT: {
+    pprod_t* t_pprod = pprod_term_desc(terms, t);
+    for (uint32_t i = 0; i < t_pprod->len; ++ i) {
+      bv_explainer_get_subtheories(exp, t_pprod->prod[i].var, subtheory_count);
+    }
+    subtheory_count[BV_TH_ARITH] ++;
+    break;
+  }
+  default:
+    // A variable or a foreign term
+    assert(is_pos_term(t));
+  }
+
+  // Mark as visited
+  int_hset_add(&exp->visited_cache, t);
+}
+
+bv_subtheory_t bv_explainer_get_subtheory(bv_explainer_t* exp, const ivector_t* conflict) {
+
+  uint32_t i;
+
+  const variable_db_t* var_db = exp->ctx->var_db;
+
+  // Reset the cache
+  int_hset_reset(&exp->visited_cache);
+
+  // Get the kinds
+  int theory_count[BV_TH_COUNT];
+  for (i = 0; i < conflict->size; i ++) {
+    term_t t = variable_db_get_term(var_db, conflict->data[i]);
+    bv_explainer_get_subtheories(exp, t, theory_count);
+  }
+
+  if (ctx_trace_enabled(exp->ctx, "mcsat::bv::conflict")) {
+    bv_subtheory_t th;
+    FILE* out = ctx_trace_out(exp->ctx);
+    fprintf(out, "subtheories:\n");
+    for (th = 0; th < BV_TH_COUNT; ++ th) {
+      if (theory_count[th] > 0) {
+        fprintf(out, "%s\n", subtheory_to_string(th));
+      }
+    }
+  }
+
   return BV_TH_FULL;
 }
 
--- src/mcsat/bv/bv_explainer.h
+++ src/mcsat/bv/bv_explainer.h
@@ -12,17 +12,28 @@
 #include "mcsat/utils/statistics.h"
 
 #include "utils/int_vectors.h"
+#include "utils/int_hash_sets.h"
 #include "terms/term_manager.h"
 
 /** Different theories that we can explain */
 typedef enum {
   /** Equality */
-  BV_TH_EQ,
+  BV_TH_EQ = 0,
   /** Equality, extraction, concatenation */
   BV_TH_EQ_EXT_CON,
-  /** Nothing in particular */
+  /** Bitwise operation (NOT, OR) */
+  BV_TH_BITWISE,
+  /** Shifting */
+  BV_TH_SHIFT,
+  /** Arithmetic comparison */
+  BV_TH_ARITH_CMP,
+  /** Arithmetic */
+  BV_TH_ARITH,
+  /** All together */
   BV_TH_FULL
 } bv_subtheory_t;
+
+#define BV_TH_COUNT (BV_TH_FULL + 1)
 
 typedef struct {
   /** Context */
@@ -31,6 +42,9 @@ typedef struct {
   term_manager_t* tm;
   /** Watch list manager */
   watch_list_manager_t* wlm;
+
+  /** Cache when visiting terms */
+  int_hset_t visited_cache;
 
   struct {
     statistic_int_t* th_eq;
@@ -50,7 +64,7 @@ void bv_explainer_destruct(bv_explainer_t* exp);
  * Returns the subtheory best suited to explain the conflict
  * @param conflict vector of mcsat variables
  * */
-bv_subtheory_t bv_explainer_get_subtheory(const bv_explainer_t* exp, const ivector_t* conflict);
+bv_subtheory_t bv_explainer_get_subtheory(bv_explainer_t* exp, const ivector_t* conflict);
 
 /**
  * Returns the conflict as explained by the given sub-theory.
