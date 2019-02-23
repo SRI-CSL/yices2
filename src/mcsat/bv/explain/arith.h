65e4b4e42aea9920e496bdc8427fea321ffe6e32
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2019-02-22 17:07:57 -0800

    Cleaning up useless comments

--- src/mcsat/bv/explain/arith.h
+++ src/mcsat/bv/explain/arith.h
@@ -11,23 +11,3 @@
 
 /** Allocate the subexplainer and setup the methods */
 bv_subexplainer_t* arith_new(plugin_context_t* ctx, watch_list_manager_t* wlm, bv_evaluator_t* eval);
-
-
-
-/* #include "mcsat/bv/bv_evaluator.h" */
-
-/* #include "mcsat/tracing.h" */
-/* #include "mcsat/mcsat_types.h" */
-
-/* #include "utils/int_vectors.h" */
-
-  
-/* /\** */
-/*  * Main function. */
-/*  * Gets a conflict core. Puts explanation in conflict */
-/*  *\/ */
-
-/* void bv_arith_get_conflict(plugin_context_t* ctx, bv_evaluator_t* eval, const ivector_t* conflict_core, variable_t conflict_var, ivector_t* conflict); */
-
-/* // Test if in fragment */
-/* bool bv_arith_applies_to(plugin_context_t* ctx, const ivector_t* conflict_core, variable_t conflict_var); */
