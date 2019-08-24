efe8afca733c152abaad950e3e5b86f2c0e78566
Alexander Nasonov <alnsn@yandex.ru>
2019-08-24 21:37:48 +0100

    Fix building on NetBSD.
     - Don't invoke static_libyices_dynamic target because
       dynamic libraries can't be built from .a files which
       aren't normally built with -fPIC.
     - POSIX shell uses "=" for comparisons rather than "==".
     - NetBSD has popcount in libc.
     - There is no ldconfig on NetBSD.

--- src/utils/bit_tricks.h
+++ src/utils/bit_tricks.h
@@ -55,6 +55,11 @@
 #include <stdint.h>
 #include <assert.h>
 
+#ifdef NETBSD
+/* NetBSD has popcount in libc. */
+#include <strings.h>
+#endif
+
 
 #ifdef __GNUC__
 
@@ -101,9 +106,11 @@ static inline uint32_t clz(uint32_t x) {
   return __builtin_clzl(x);
 }
 
+#ifndef NETBSD
 static inline uint32_t popcount32(uint32_t x) {
   return __builtin_popcountl(x);
 }
+#endif
 
 #else
 //#warning "ctz: uint32_t is (unsigned int)"
@@ -118,9 +125,11 @@ static inline uint32_t clz(uint32_t x) {
   return __builtin_clz(x);
 }
 
+#ifndef NETBSD
 static inline uint32_t popcount32(uint32_t x) {
   return __builtin_popcount(x);
 }
+#endif
 
 #endif
 
@@ -141,9 +150,11 @@ static inline uint32_t clz64(uint64_t x) {
   return __builtin_clzll(x);
 }
 
+#ifndef NETBSD
 static inline uint32_t popcount64(uint64_t x) {
   return __builtin_popcountll(x);
 }
+#endif
 
 #else
 // #warning "bit_tricks: uint64_t is (unsigned long)
@@ -158,9 +169,11 @@ static inline uint32_t clz64(uint64_t x) {
   return __builtin_clzl(x);
 }
 
+#ifndef NETBSD
 static inline uint32_t popcount64(uint64_t x) {
   return __builtin_popcountl(x);
 }
+#endif
 
 #endif // 64bit versions
 
@@ -223,6 +236,7 @@ static inline uint32_t clz64(uint64_t x) {
   return i;
 }
 
+#ifndef NETBSD
 static inline uint32_t popcount32(uint32_t x) {
   uint32_t c;
 
@@ -246,6 +260,7 @@ static inline uint32_t popcount64(uint64_t x) {
 
   return c;
 }
+#endif /* #ifndef NETBSD */
 
 #endif
 
