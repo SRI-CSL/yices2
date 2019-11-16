c2cc844331fa47fadc67f943dd69c88efa9df0ab
Bruno Dutertre <bruno@csl.sri.com>
2019-11-15 20:38:56 -0800

    Fixed comments + reformatting

--- src/terms/bv_constants.c
+++ src/terms/bv_constants.c
@@ -94,13 +94,17 @@ static void resize_allocator(bvconst_allocator_t *s, uint32_t n) {
 
   assert(s->nstores <= n && n < MAX_NUM_STORES);
 
-  // new_size = max(s->nstores * 1.5, n+1)
+  // new_size = max(s->nstores * 1.5, n+1, MAX_NUM_STORES)
   new_size = s->nstores + 1;
   new_size += new_size >> 1;
-  if (new_size <= n) new_size = n + 1;
-  if (new_size > MAX_NUM_STORES) new_size = MAX_NUM_STORES;
-  assert(new_size <= MAX_NUM_STORES);
-    
+  if (new_size <= n) {
+    new_size = n + 1;
+  } else if (new_size > MAX_NUM_STORES) {
+    new_size = MAX_NUM_STORES;
+  }
+
+  assert(new_size <= MAX_NUM_STORES && new_size >= n+1);
+
   s->store = (object_store_t *) safe_realloc(s->store, new_size * sizeof(object_store_t));
   for (i=s->nstores; i<new_size; i++) {
     init_objstore(s->store + i, (2 * i) * sizeof(uint32_t), BVCONST_BANK_SIZE);
