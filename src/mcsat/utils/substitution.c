b6f1b5b208bafe2247b057c5c56dd3e5a3402e55
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-07-06 05:02:36 -0400

    [assumptions] Add missing cases for term substitution (wasn't used before interpolation).
    Fixes #261.

--- src/mcsat/utils/substitution.c
+++ src/mcsat/utils/substitution.c
@@ -274,6 +274,75 @@ term_t substitution_run_core(substitution_t* subst, term_t t, int_hmap_t* cache,
       break;
     }
 
+    case ARITH_GE_ATOM: {
+      term_t arg = arith_ge_arg(terms, current);
+      find = int_hmap_find(cache, arg);
+      if (find == NULL) {
+        ivector_push(&substitution_stack, arg);
+      } else {
+        if (find->val == arg) {
+          current_subst = current;
+        } else {
+          current_subst = arith_geq_atom(terms, find->val);
+        }
+      }
+      break;
+    }
+
+    case ARITH_EQ_ATOM: {
+      term_t arg = arith_eq_arg(terms, current);
+      find = int_hmap_find(cache, arg);
+      if (find == NULL) {
+        ivector_push(&substitution_stack, arg);
+      } else {
+        if (find->val == arg) {
+          current_subst = current;
+        } else {
+          current_subst = arith_eq_atom(terms, find->val);
+        }
+      }
+      break;
+    }
+
+    case ARITH_POLY:
+    {
+      polynomial_t* p = poly_term_desc(terms, current);
+      n = p->nterms;
+
+      bool children_done = true;
+      bool children_same = true;
+
+      ivector_t children;
+      init_ivector(&children, n);
+      for (i = 0; i < n; ++ i) {
+        term_t x = p->mono[i].var;
+        if (x != const_idx) {
+          find = int_hmap_find(cache, x);
+          if (find == NULL) {
+            children_done = false;
+            ivector_push(&substitution_stack, x);
+          } else if (find->val != x) {
+            children_same = false;
+          }
+          if (children_done) { ivector_push(&children, find->val); }
+        } else {
+          if (children_done) { ivector_push(&children, const_idx); }
+        }
+      }
+
+      if (children_done) {
+        if (children_same) {
+          current_subst = current;
+        } else {
+          current_subst = mk_arith_poly(tm, p, n, children.data);
+        }
+      }
+
+      delete_ivector(&children);
+
+      break;
+    }
+
     case BIT_TERM: // bit-select current = child[i]
     {
       uint32_t index = bit_term_index(terms, current);
@@ -290,6 +359,7 @@ term_t substitution_run_core(substitution_t* subst, term_t t, int_hmap_t* cache,
       }
       break;
     }
+
     case BV_POLY:  // polynomial with generic bitvector coefficients
     {
       bvpoly_t* p = bvpoly_term_desc(terms, current);
