1634b69796a758f7362d0e85280314bb8f29496a
Aina Niemetz <aina.niemetz@gmail.com>
2020-08-31 14:52:55 -0700

    yices.h: Fix typo in documentation of yices_check_context_with_model.

--- src/include/yices.h
+++ src/include/yices.h
@@ -3170,7 +3170,7 @@ __YICES_DLLSPEC__ extern smt_status_t yices_check_context_with_assumptions(conte
  * - t = variables to use, i.e., we check context && t = mdl(t)
  * - the variables t[0] ... t[n-1] must all be uninterpreted terms
  *
- * It behaves the same as the previous function. Note that if the model will take
+ * It behaves the same as the previous function. Note that the model will take
  * default values for variables in t that are not explicitly defined.
  *
  * If this function returns STATUS_UNSAT, then one can construct a model interpolant by
