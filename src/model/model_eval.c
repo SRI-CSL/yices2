9dce84c0c8223089c152146a9caba8790b16d0bd
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-10-13 15:19:23 -0400

    [mcsat] eval_arith_ge

--- src/model/model_eval.c
+++ src/model/model_eval.c
@@ -214,7 +214,17 @@ static value_t eval_arith_ge(evaluator_t *eval, term_t t) {
   value_t v;
 
   v = eval_term(eval, t);
-  return vtbl_mk_bool(eval->vtbl, q_is_nonneg(eval_get_rational(eval, v)));
+
+  if (object_is_rational(eval->vtbl, v)) {
+    return vtbl_mk_bool(eval->vtbl, q_is_nonneg(vtbl_rational(eval->vtbl, v)));
+  } else {
+#ifdef HAVE_MCSAT
+    return vtbl_mk_bool(eval->vtbl, lp_algebraic_number_sgn(vtbl_algebraic_number(eval->vtbl, v)) >= 0);
+#else
+    assert(false);
+    return MDL_EVAL_INTERNAL_ERROR;
+#endif
+  }
 }
 
 /*
