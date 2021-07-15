56136053f61f04b9b7e4c9ae2f8279f490cd9f3b
Ian A.Mason <iam@csl.sri.com>
2021-07-15 08:46:35 -0700

    Make the thing mergable into master

--- src/api/yices_thread_local.h
+++ src/api/yices_thread_local.h
@@ -5,7 +5,20 @@
  *
  */
 #ifdef THREAD_SAFE
+/*
+ * Generic thread local
+ */
 #define YICES_THREAD_LOCAL __thread
+/*
+ * Specific to PER_THREAD_STATE
+ */
+#ifdef PER_THREAD_STATE
+#define YICES_PTS_LOCAL __thread
+#else
+#define YICES_PTS_LOCAL
+#endif
 #else
 #define YICES_THREAD_LOCAL
 #endif
+
+
