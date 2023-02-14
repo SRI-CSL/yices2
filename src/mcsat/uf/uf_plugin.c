b82d1a669d14e8d67b6ac45ff2246251b94d9c40
Ahmed Irfan <ahmed.irfan@sri.com>
2023-02-14 01:35:08 -0800

    opt

--- src/mcsat/uf/uf_plugin.c
+++ src/mcsat/uf/uf_plugin.c
@@ -745,9 +745,9 @@ void uf_plugin_add_to_eq_graph(uf_plugin_t* uf, term_t t, bool record) {
     term_t r1 = app_term(terms, t, t_desc->arity - 2, t_desc->arg + 1);
     variable_db_get_variable(uf->ctx->var_db, r1);
     ivector_push(&uf->select_terms, r1);
-    term_t r2 = app_term(terms, t_desc->arg[0], t_desc->arity - 2, t_desc->arg + 1);
-    variable_db_get_variable(uf->ctx->var_db, r2);
-    ivector_push(&uf->select_terms, r2);
+    /* term_t r2 = app_term(terms, t_desc->arg[0], t_desc->arity - 2, t_desc->arg + 1); */
+    /* variable_db_get_variable(uf->ctx->var_db, r2); */
+    /* ivector_push(&uf->select_terms, r2); */
     break;
   case ARITH_RDIV:
     t_desc = arith_rdiv_term_desc(terms, t);
@@ -792,16 +792,6 @@ void uf_plugin_add_to_eq_graph(uf_plugin_t* uf, term_t t, bool record) {
       if (eq_graph_term_is_rep(&uf->eq_graph, c)) {
         eq_graph_propagate_trail_assertion(&uf->eq_graph, c);
       }
-    }
-    if (is_function_term(terms, c)) {
-      if (USE_ARRAY_DIFF) {
-        uf_plugin_add_diff_terms_vars(uf, c);
-      }
-      ivector_push(&uf->array_terms, c);
-    }
-    if (term_kind(terms, c) == APP_TERM &&
-        !int_hset_member(&uf->diff_funs, c)) {
-      ivector_push(&uf->select_terms, c);
     }
   }
 
