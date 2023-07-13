dc3c6c745e20e0146b380e15958f288c0a8f7f34
Ahmed Irfan <ahmed.irfan@sri.com>
2023-07-12 18:04:21 -0700

    rm push/pop comment about mcsat

--- src/include/yices.h
+++ src/include/yices.h
@@ -2654,8 +2654,7 @@ __YICES_DLLSPEC__ extern void yices_garbage_collect(const term_t t[], uint32_t n
  * - mcsat: solver based on the Model-Constructing Satisfiability Calculus
  *
  * The "mcsat" solver is required for formulas that use non-linear
- * arithmetic. Currently the mcsat solver does not support push and
- * pop. If you select "mcsat" as the solver type, no other
+ * arithmetic. If you select "mcsat" as the solver type, no other
  * configuration is necessary.
  *
  * If you select "dpllt" as the solver type, then you can define the
