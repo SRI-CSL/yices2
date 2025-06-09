1c6db712dc494b8540fdcdf53eaff50085fa77d9
Ahmed Irfan <43099566+ahmed-irfan@users.noreply.github.com>
2025-06-09 01:13:20 -0700

    fix type macros test (#561)

--- tests/api/type_macros.c
+++ tests/api/type_macros.c
@@ -154,7 +154,8 @@ void check_yices_macro_names(void)
 
     yices_delete_type_macro(tm0);
     type_t types_0[1] = { yices_bool_type() };
-    yices_instance_type(tm0, 1, types_0);
+    tm0_get = yices_get_macro_by_name("tm0");
+    yices_instance_type(tm0_get, 1, types_0);
     assert(yices_error_code());
     yices_clear_error();
 
